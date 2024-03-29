0a625061845e2abad0cfa51d8a5577b2921acbff
fabriciob.coimbra <fabriciob.coimbra@outlook.com>
2024-03-29 03:40:42 -0300

    Activate AOT and add doc XML, but the controllers don't works kkkk

--- src/HelloWorld.API/Program.cs
+++ src/HelloWorld.API/Program.cs
@@ -1,4 +1,5 @@
 using HelloWorld.API.Service;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,7 +7,21 @@ builder.Services.AddScoped<IEmployeeSalaryService, EmployeeSalaryService>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Simple API",
+        Description = "Repository of concepts basics and advance from my life experience."
+    });
+
+    foreach (var filePath in Directory.GetFiles(AppContext.BaseDirectory, "*.xml"))
+    {
+        if (File.Exists(filePath))
+            c.IncludeXmlComments(filePath);
+    }    
+});
 
 var app = builder.Build();
 
