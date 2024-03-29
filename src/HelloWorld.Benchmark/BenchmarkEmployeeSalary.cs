15dfd6ce16e12c3e52ec7ce99d0c6fe336988867
fabriciob.coimbra <fabriciob.coimbra@outlook.com>
2024-03-29 02:38:23 -0300

    Adicionado Benchmark da primeira versão.

---
+++ src/HelloWorld.Benchmark/BenchmarkEmployeeSalary.cs
@@ -0,0 +1,16 @@
+﻿using BenchmarkDotNet.Attributes;
+using HelloWorld.API.Service;
+
+namespace HelloWorld.Benchmark;
+
+[MemoryDiagnoser]
+public class BenchmarkEmployeeSalary
+{
+    private readonly IEmployeeSalaryService _employeeSalaryService = new EmployeeSalaryService();
+
+    [Benchmark]
+    public void MeasureAnualTotalSalary()
+    {
+        _employeeSalaryService.GetAnualTotalSalary();
+    }
+}
---
+++ src/HelloWorld.Benchmark/Program.cs
@@ -0,0 +1,5 @@
+﻿using BenchmarkDotNet.Running;
+using HelloWorld.Benchmark;
+
+Console.WriteLine("Hello, World! Starting Benchmark");
+var summary = BenchmarkRunner.Run<BenchmarkEmployeeSalary>();
