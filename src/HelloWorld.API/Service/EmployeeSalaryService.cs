d4ca61e356e3d900d1d14a777d881efaa1e5518d
fabriciob.coimbra <fabriciob.coimbra@outlook.com>
2024-03-29 03:21:57 -0300

    Used simplified array initialization to reduce lines of code. It reduced memory usage and increase performance time.

--- src/HelloWorld.API/Service/EmployeeSalaryService.cs
+++ src/HelloWorld.API/Service/EmployeeSalaryService.cs
@@ -8,26 +8,12 @@ public partial class EmployeeSalaryService : IEmployeeSalaryService
     {
         return Seed().Sum(e => e.Salary);
     }
-    private List<IEmployee> Seed()
-    {
-        List<IEmployee> result = [];
-
-        IEmployee teatecher1 = EmployeeFactory.GetEmployeeInstance(EmployeeType.Teacher, 1, "Bob Fisher", 40000, 28);
-        result.Add(teatecher1);
-
-        IEmployee teatecher2 = EmployeeFactory.GetEmployeeInstance(EmployeeType.Teacher, 2, "Thomas", 40000, 32);
-        result.Add(teatecher2);
-
-        IEmployee headOfDepartment = EmployeeFactory.GetEmployeeInstance(EmployeeType.HeadOfDepartment, 3, "Brenda", 50000, 35);
-        result.Add(headOfDepartment);
-
-        IEmployee deputyHeadMaster = EmployeeFactory.GetEmployeeInstance(EmployeeType.DeputyHeadMaster, 4, "Devlin", 60000, 28);
-        result.Add(deputyHeadMaster);
-
-        IEmployee headMaster = EmployeeFactory.GetEmployeeInstance(EmployeeType.HeadMaster, 5, "Damien", 80000, 28);
-        result.Add(headMaster);
-
-        return result;
-    }
-
+    private static List<IEmployee> Seed() =>
+    [
+        EmployeeFactory.GetEmployeeInstance(EmployeeType.Teacher, 1, "Bob Fisher", 40000, 28),
+        EmployeeFactory.GetEmployeeInstance(EmployeeType.Teacher, 2, "Thomas", 40000, 32),
+        EmployeeFactory.GetEmployeeInstance(EmployeeType.HeadOfDepartment, 3, "Brenda", 50000, 35),
+        EmployeeFactory.GetEmployeeInstance(EmployeeType.DeputyHeadMaster, 4, "Devlin", 60000, 28),
+        EmployeeFactory.GetEmployeeInstance(EmployeeType.HeadMaster, 5, "Damien", 80000, 28)
+    ];
 }
